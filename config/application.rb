1a0f20a872d38d6f39bf82438fc0fb8fcb1300ea
sumar7 <sajjadumardev@gmail.com>
2020-10-22 16:04:45 +0500

    added classic autoloader

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module VestaRails
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.autoloader = :classic
 
     # CORS
     config.middleware.use Rack::Cors do
