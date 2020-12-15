eafffc98966a0781d40762f6a8162ef3d51fdf4d
sumar7 <sajjadumardev@gmail.com>
2020-12-15 11:06:54 +0500

    Added functionality of fetching name form first linked account holder_name attribute.

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,15 @@ class Account < ApplicationRecord
   belongs_to :user
 
   has_many :saved_transactions, dependent: :destroy
+
+  after_create :update_username
+
+  private
+
+  def update_username
+    if user.first_name&.empty?
+      name = holder_name&.split(' ', 2)
+      user.update_columns(first_name: name.first, surname: name.last)
+    end
+  end
 end
