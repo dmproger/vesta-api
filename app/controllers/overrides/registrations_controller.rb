cae89d8e883d477f518b39b3065e71daeb0a3036
dmitryck <dmitryck@gmail.com>
2021-04-29 22:19:17 +0300

    fix backwards in registartion;

--- app/controllers/overrides/registrations_controller.rb
+++ app/controllers/overrides/registrations_controller.rb
@@ -7,9 +7,14 @@ module Overrides
     def create
       create_params = sign_up_params.dup
       email = create_params.delete(:email)&.downcase
-      @resource = User.new(email: email, uid: email,
-                           password: Devise.friendly_token.first(8),
-                           provider: 'email')
+
+      @resource = params[:id] ? User.find(params[:id]) : User.new
+
+      @resource.email     = email
+      @resource.uid       = email
+      @resource.password  = Devise.friendly_token.first(8)
+      @resource.provider  = 'email'
+
       @resource.assign_attributes(create_params)
       @resource.save ? render_create_success : render_create_error
     end
