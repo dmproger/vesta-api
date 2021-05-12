535ed4eafd0f99e2e6c52df8732a1273b6d85106
dmitryck <dmitryck@gmail.com>
2021-05-12 21:44:30 +0300

    deploy: staging indian;

--- config/deploy/staging_indian.rb
+++ config/deploy/staging_indian.rb
@@ -1,8 +1,9 @@
-lock "~> 3.14.1"
+server '35.177.182.142', user: 'ubuntu', roles: %w{web app db}
 
 set :application, 'vesta-rails'
 set :repo_url, 'git@github.com:dmproger/vesta-api.git'
-set :branch, :master
+# set :repo_url, 'git@github.com:vesta-tech/rails.git'
+set :branch, :indian_version
 set :deploy_to, '/home/ubuntu/vesta-rails'
 set :pty, true
 set :linked_files, %w{config/database.yml}
@@ -11,32 +12,32 @@ set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.7.0'
 
+set :default_env, { 'SANDBOX_ENV' => 'false' }
+
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
-set :puma_env, 'production'
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [0, 8]
 set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
 
+
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+
 set :delayed_job_workers
 set :delayed_job_roles, %i(app background)
 
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:start'
-    end
-  end
-end
-
-after 'deploy', 'deploy:restart'
+set :ssh_options, {
+    keys: %w(vesta-dev-rails.pem),
+    forward_agent: true,
+    auth_methods: %w(publickey)
+}
