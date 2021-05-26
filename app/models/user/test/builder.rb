d004ba6dcd69f93aefb9049f42424e918f695434
dmitryck <dmitryck@gmail.com>
2021-05-26 16:11:40 +0300

    model: test user models dinamyc done;

--- app/models/user/test/builder.rb
+++ app/models/user/test/builder.rb
@@ -1,19 +1,42 @@
-module User::Test::Builder
-  PHONES = ENV['PHONES']&.split(' ') || %w[+447768333333]
+class User
+  module Test
+    module Builder
+      PHONES = ENV['PHONES']&.split(' ') || \
+        %w[+447768333333]
 
-  def self.build
-    for phone in PHONES
-      user = User.find_by(phone: phone) || User.joins(:saved_transactions).first
-      user.saved_transactions.index_by(&:account).each do |account, transaction|
-        create_class(user, account, transaction)
+      class << self
+        def build
+          for @phone, @i in PHONES.each_with_index
+            @user = User.find_by(phone: @phone) || User.joins(:saved_transactions).first
+            for @account, @transactions in @user.saved_transactions.group(:id, :account_id).each_with_object({}) { |r, o| (o[r.account] ||= [])<< r }
+              @i += 1
+              @model = create_model
+              config_model
+            end
+          end
+        end
+
+        def create_model
+          @model_name = model_name
+          superclass = Class.new(ActiveRecord::Base)
+          User::Test.const_set(@model_name, superclass)
+
+          "User::Test::#{ @model_name }".constantize
+        end
+
+        def config_model
+          @tids = @transactions.pluck(:id)
+          @model.class_eval <<-STR
+            self.table_name = 'saved_transactions'
+            default_scope { where(id: #{ @tids }) }
+          STR
+        end
+
+        def model_name
+          client = @account.holder_name.gsub(/\s*/, '').classify
+          "#{ client }_Bank#{ @i }"
+        end
       end
     end
-  end
-
-  def self.create_class(user, account, transaction)
-    klass_name = account.holder_name.gsub(/\s*/, '')
-    klass = Class.new(ActiveRecord::Base)
-    byebug
-    Object.const_set("#{ self }::#{ klass_name }", klass)
   end
 end
