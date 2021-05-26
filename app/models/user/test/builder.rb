1b88f81c1f91a56d2283f2ddd319feb4174dea96
dmitryck <dmitryck@gmail.com>
2021-05-26 18:42:38 +0300

    model/config: test user build logic done;

--- app/models/user/test/builder.rb
+++ app/models/user/test/builder.rb
@@ -5,13 +5,18 @@ class User
         %w[+447768333333]
 
       class << self
+        attr_reader :models
+
         def build
+          @models = []
+
           for @phone, @i in PHONES.each_with_index
             @user = User.find_by(phone: @phone) || User.joins(:saved_transactions).first
             for @account, @transactions in @user.saved_transactions.group(:id, :account_id).each_with_object({}) { |r, o| (o[r.account] ||= [])<< r }
               @i += 1
               @model = create_model
               config_model
+              @models << @model
             end
           end
         end
@@ -24,17 +29,24 @@ class User
         end
 
         def config_model
-          @tids = @transactions.pluck(:id)
+          @model.class_eval do
+            self.table_name = 'saved_transactions'
+            before_save do |record|
+              defaults = self.class.first.attributes.delete_if { |k, v| k == 'id' }
+              current = record.attributes.keep_if { |_, v| !v.nil? }
+
+              record.assign_attributes(defaults.merge(current))
+            end
+          end
 
           @model.class_eval <<-STR
-            self.table_name = 'saved_transactions'
-            default_scope { where(id: #{ @tids }) }
+            default_scope { where(account_id: '#{ @account.id }') }
           STR
         end
 
         def model_name
           client = @account.holder_name.gsub(/\s*/, '').classify
-          "#{ client }_Bank#{ @i }"
+          "TestUserBank#{ @i }"
         end
       end
     end
