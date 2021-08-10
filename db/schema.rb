06cd044baac5d33e0cc4180fba130c1735e1e19d
zlatov <zlatov@ya.ru>
2021-08-10 10:15:33 +0300

    rails db:migrate RAILS_ENV=development

--- db/schema.rb
+++ db/schema.rb
@@ -70,16 +70,6 @@ ActiveRecord::Schema.define(version: 2021_08_04_145902) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "associated_transactions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "property_tenant_id"
     t.uuid "saved_transaction_id"
