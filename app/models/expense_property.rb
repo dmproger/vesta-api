553365327b7c26d0415cc96084cd7ff5ad661e2d
dmitryck <dmitryck@gmail.com>
2021-08-11 18:33:48 +0300

    model/spec: in progress..;

--- app/models/expense_property.rb
+++ app/models/expense_property.rb
@@ -2,4 +2,7 @@ class ExpenseProperty < ApplicationRecord
   belongs_to :expense
   belongs_to :property
   belongs_to :saved_transaction
+
+  # TODO
+  # db validate uniq expense, saved_transaction
 end
