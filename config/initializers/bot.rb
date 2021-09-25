1443f25d9576d4b3fb9998063e98b01a7d3c1efa
dmitryck <dmitryck@gmail.com>
2021-09-25 14:04:48 +0300

    init: bot every 30 min;

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -1,6 +1,6 @@
 require_relative '../../app/delayed_jobs/bot/tink_job'
 
-INTERVAL_IN_MINUTES = 10
+INTERVAL_IN_MINUTES = 30
 
 Delayed::Job.enqueue(
   Bot::TinkJob.new, cron: "*/#{ INTERVAL_IN_MINUTES } * * * *"
