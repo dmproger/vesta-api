a75d3cc2aacf99ea98bc96c5630e853acf718a84
dmitryck <dmitryck@gmail.com>
2021-10-31 18:29:11 +0300

    api: message actions;

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,20 +1,28 @@
 class Api::V1::MessagesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_messages
+  before_action :set_messages, only: [:index]
+  before_action :set_message, only: [:update, :destroy]
 
   def index
+    render json: { success: true, data: @messages }
   end
 
   def show
+    render json: { success: true, data: @message }
   end
   
   def create
+    message = Message.create!(message_params.merge(user: current_user))
+    render json: { success: true, data: message }
   end
 
   def update
+    @message.update!(message_params)
+    render json: { success: true, data: @message.reload }
   end
 
   def destroy
+    @message.destroy!
   end
 
   private
@@ -22,5 +30,13 @@ class Api::V1::MessagesController < ApplicationController
   def set_messages
     type = params[:type] || ''
     @messages = current_user.send("#{type}messages")
+  end
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.permit(:topic, :text, images: [])
   end
 end
