2e54b074084bc853ebc9ce5ac81c415859d95970
dmitryck <dmitryck@gmail.com>
2021-10-31 20:17:12 +0300

    model: reciver optional in messages;

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :user
-  belongs_to :reciver, class_name: 'User', foreign_key: :reciver
+  belongs_to :reciver, class_name: 'User', foreign_key: :reciver, optional: true
 
   enum kind: { to_support: 1 }
 
   has_many_attached :images
 
-  scope from: ->(user){ where(user: user) }
-  scope to: ->(user){ where(reciver: user) }
+  scope :form_user, ->(user){ where(user: user) }
+  scope :to_user, ->(user){ where(reciver: user) }
 end
