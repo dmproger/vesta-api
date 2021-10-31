2bd27c6a1895ccab02def2dce784c5fcab9f5700
dmitryck <dmitryck@gmail.com>
2021-10-31 20:02:58 +0300

    model: kind enum messages;

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :reciver, class_name: 'User', foreign_key: :reciver
 
+  enum kind: { to_support: 1 }
+
   has_many_attached :images
 
   scope from: ->(user){ where(user: user) }
