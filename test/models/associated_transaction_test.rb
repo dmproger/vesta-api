4160f1ee1d9fd4646f0ab00dfaa217e7a996362a
dmitryck <dmitryck@gmail.com>
2021-11-01 17:52:29 +0300

    test: remove empty test folder;

--- test/models/associated_transaction_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AssociatedTransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/property_tenant_transaction_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PropertyTenantTransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
