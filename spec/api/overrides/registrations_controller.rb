e332c151a68f3a2f1e4fe710dfaaee1b799bfa10
dmproger <dmproger@gmail.com>
2021-12-20 19:45:35 +0300

    spec: send_otp registration test;

--- spec/api/overrides/registrations_controller.rb
+++ spec/api/overrides/registrations_controller.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
+ENV.delete('DO_NOT_SEND_SMS') if ENV['TWILLIO_TEST']
+
 RSpec.describe Overrides::RegistrationsController do
   describe 'create user' do
     subject { post '/api/v1/auth/', headers: headers, params: params }
 
-    let(:user) { build(:user) }
+    let(:user) { build(:user, phone: ENV['TWILLIO_TEST_PHONE']) }
     let(:params) { user.attributes }
     let(:headers) { {} }
 
